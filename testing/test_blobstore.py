a1f56dd7c55580d54343960977e884e40e87cab3
Ronny Pfannschmidt <opensource@ronnypfannschmidt.de>
2010-12-15 14:12:54 +0100

    propper contains checks

--- testing/test_blobstore.py
+++ testing/test_blobstore.py
@@ -1,18 +1,27 @@
 import py
-from bmst import FileStore
+from bmst import FileStore, MappingStore
+
+
+def pytest_generate_tests(metafunc):
+    if 'store' in metafunc.funcargnames:
+        metafunc.addcall(id='file', param=file)
+        metafunc.addcall(id='map', param=map)
 
 def pytest_funcarg__store(request):
-    return FileStore(request.getfuncargvalue('tmpdir'))
+    if request.param is file:
+        return FileStore(request.getfuncargvalue('tmpdir'))
+    if request.param is map:
+        return MappingStore({})
 
-def test_put(store, tmpdir):
-    hash = py.std.hashlib.sha1('test').hexdigest()
-    key = store.put(hash, 'test')
-    assert tmpdir.join(hash).check()
+def test_store(store, tmpdir):
+    key = py.std.hashlib.sha1('test').hexdigest()
+    store.put(key, 'test')
+    assert key in store
 
 
 
 def test_get(store, tmpdir):
-    hash = py.std.hashlib.sha1('test').hexdigest()
-    test_put(store, tmpdir)
-    assert store.get(hash) == 'test'
+    key = py.std.hashlib.sha1('test').hexdigest()
+    test_store(store, tmpdir)
+    assert store.get(key) == 'test'
 
