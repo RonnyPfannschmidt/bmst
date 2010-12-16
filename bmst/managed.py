d975218d95c0b3fba546d9c7276cd18d814a650c
Ronny Pfannschmidt <Ronny.Pfannschmidt@gmx.de>
2010-12-16 10:53:26 +0100

    combined store blob puting

--- bmst/managed.py
+++ bmst/managed.py
@@ -3,6 +3,10 @@ from bmst.metastore import find_missing_blobs
 import bz2
 import hashlib
 
+
+def sha1(data):
+    return hashlib.sha1(data).hexdigest()
+
 class Combined(object):
     def __init__(self, compression, store, root):
         self.meta = store(root, 'meta')
@@ -10,9 +14,29 @@ class Combined(object):
         self.compression = compression
 
 
-    def put_meta(self, key, mapping):
+    def put_meta(self, key=None, mapping=None):
+        raw_data = json.dumps(mapping, indent=2, sort_keys=True)
+        computed_key = sha1(raw_data)
+        if key is None:
+            key = computed_key
+        elif computed_key != key:
+            raise ValueError
+
         missing = find_missing_blobs(mapping, self.blobs)
         if missing:
             raise LookupError(missing)
 
-        raw_data = json.dumps(mapping, indent=2, sort_keys=True)
+        self.meta[key] = self.compression.compress(raw_data)
+
+        return key
+
+    def put_blob(self, key=None, data=None):
+        computed_key = sha1(data)
+        if key is None:
+            key = computed_key
+        elif computed_key != key:
+            raise ValueError
+
+        self.blobs[key] = self.compression.compress(data)
+        return key
+
--- testing/test_manager.py
+++ testing/test_manager.py
@@ -16,12 +16,18 @@ def pytest_funcarg__store(request):
 def should_fail_put_meta_on_missing_blob(store):
 
     with py.test.raises(LookupError) as excinfo:
-        store.put_meta('test', {
+        store.put_meta(mapping={
             'items': {
                 'test': '123',
             },
         })
 
 
-def should_put_meta_on_existing_blob(store):
-    pass
+def should_put_meta_on_existing_blob(store, mapping):
+    blob = store.put_blob(data='test')
+    store.put_meta(mapping={
+        'items': {
+            'test': blob,
+        },
+    })
+
