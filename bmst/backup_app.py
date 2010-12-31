f61b2ddc927f57b92ae8b09dfe9b5e015b5172f3
Ronny Pfannschmidt <opensource@ronnypfannschmidt.de>
2010-12-31 14:53:40 +0100

    add simple meta guessing

--- bmst/backup_app.py
+++ bmst/backup_app.py
@@ -1,5 +1,26 @@
 
 
+def fullmeta(root):
+    guessed = guessmeta(root)
+    items = load_tree(root)
+
+    item_meta = {}
+    blobs = []
+    for k, (hash, content) in items.iteritems():
+        item_meta[k] = hash
+        blobs.append((hash, content))
+
+    guessed['items'] = item_meta
+    return guessed, blobs
+
+
+def guessmeta(root):
+    return {
+        'project': root.basename,
+        'tags': ['backup'],
+    }
+
+
 def load_tree(root):
     results = {}
     for x in root.visit():
--- testing/test_backup_app.py
+++ testing/test_backup_app.py
@@ -1,4 +1,4 @@
-from bmst.backup_app import load_tree
+from bmst.backup_app import load_tree, fullmeta
 
 
 def test_load(tmpdir):
@@ -9,3 +9,20 @@ def test_load(tmpdir):
     assert load_tree(tmpdir) == {
         'a/b': ('4e1243bd22c66e76c2ba9eddc1f91394e57f9f83', 'test\n'),
     }
+
+
+def test_fullmeta(tmpdir):
+    root = tmpdir.ensure('root', dir=1)
+    root.join('test').write('test\n')
+    meta, blobs = fullmeta(root)
+    assert meta == {
+        'project': 'root',
+        'tags': ['backup'],
+        'items': {
+            'test': '4e1243bd22c66e76c2ba9eddc1f91394e57f9f83',
+        },
+    }
+
+    assert blobs == [
+        ('4e1243bd22c66e76c2ba9eddc1f91394e57f9f83', 'test\n'),
+    ]
