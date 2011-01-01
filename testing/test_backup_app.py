98588e0e9e14c9e260b784b1f87d80a882d5ce09
Ronny Pfannschmidt <Ronny.Pfannschmidt@gmx.de>
2011-01-01 15:21:07 +0100

    first test for makebackup

--- testing/test_backup_app.py
+++ testing/test_backup_app.py
@@ -1,4 +1,7 @@
-from bmst.backup_app import load_tree, fullmeta
+from bmst.backup_app import load_tree, fullmeta, make_backup
+from bmst.managed import  BMST
+from bmst import MappingStore
+import bz2
 
 
 def test_load(tmpdir):
@@ -23,6 +26,24 @@ def test_fullmeta(tmpdir):
         },
     }
 
-    assert blobs == [
-        ('4e1243bd22c66e76c2ba9eddc1f91394e57f9f83', 'test\n'),
-    ]
+    assert blobs == {
+        '4e1243bd22c66e76c2ba9eddc1f91394e57f9f83': 'test\n',
+    }
+
+
+def test_makebackup(tmpdir):
+    test_fullmeta(tmpdir)
+    bmst = BMST(bz2, MappingStore, {})
+    make_backup(tmpdir.join('root'), bmst)
+    assert bmst.root == {
+        ('blobs', '4e1243bd22c66e76c2ba9eddc1f91394e57f9f83'):
+            'BZh91AY&SY\xcc\xc3q\xd4\x00\x00\x02A\x80\x00\x10\x02\x00\x0c\x00'
+            ' \x00!\x9ah3M\x19\x97\x8b\xb9"\x9c(Hfa\xb8\xea\x00',
+        ('meta', '675857289b324a4f65e1fc5ead1a97d75a248945'):
+            'BZh91AY&SY(\xe2\xd0f\x00\x00?\xdb\x80@\x10P\x04?\xf0\x00\n?\xba'
+            '\xde\n \x00t"\x9az\x02z\x9a1\x03\xd4h\x0c\x83S\xd2CC \xd0\x1ah\r'
+            '\x04\x17z\x10N\x1e\x81\x80\x80\xc5)\xe8\t$\x9e\x9d%\x91 \xe5\x80'
+            '~lt\xda8&F:\x0bX\xd6\x19\x89\xcc%\xc5\xae\x00\x88L3zY\x12K\x89'
+            '\xaa\xb7\x10_p Das\xfe\x91P\x06\xaa\xdd\x1e\xd5\x8b\x9b\x97DT'
+            '\xf9G\x9f\x8b\xb9"\x9c(H\x14qh3\x00',
+    }
