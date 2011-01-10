ef97aeccc5327c77fc9146cdecb35f590679771b
Ronny Pfannschmidt <opensource@ronnypfannschmidt.de>
2011-01-10 11:27:57 +0100

    some python3 compat work

--- bmst/backup_app.py
+++ bmst/backup_app.py
@@ -7,7 +7,7 @@ def fullmeta(root):
 
     item_meta = {}
     blobs = {}
-    for k, (hash, content) in items.iteritems():
+    for k, (hash, content) in items.items():
         item_meta[k] = hash
         # asume collisions are unlikely enough
         blobs[hash] = content
@@ -27,7 +27,7 @@ def load_tree(root):
     results = {}
     for x in root.visit():
         if x.check(file=1):
-            results[x.relto(root)] = x.computehash('sha1'), x.read()
+            results[x.relto(root)] = x.computehash('sha1'), x.read('rb')
     return results
     results = {}
 
@@ -38,7 +38,7 @@ def make_backup(root, bmst):
         return bmst.put_meta(mapping=meta)
     except LookupError as e:
         missing_mapping = e.args[0]
-        for key in missing_mapping.itervalues():
+        for key in missing_mapping.values():
             bmst.put_blob(key=key, data=blobs[key])
 
         return bmst.put_meta(mapping=meta)
--- bmst/metastore.py
+++ bmst/metastore.py
@@ -4,7 +4,7 @@ import collections
 
 def find_missing_blobs(data, blobstore):
     missing = {}
-    for name, value in data['items'].iteritems():
+    for name, value in data['items'].items():
         if value not in blobstore:
             missing[name] = value
     if missing:
