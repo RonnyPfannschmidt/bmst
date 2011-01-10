2dd39485d08414ed37b991fdddf8c7818db6482a
Ronny Pfannschmidt <Ronny.Pfannschmidt@gmx.de>
2011-01-10 20:32:28 +0100

    use keys and set ops for finding missing blobs

--- bmst/metastore.py
+++ bmst/metastore.py
@@ -3,9 +3,10 @@ import collections
 
 
 def find_missing_blobs(data, blobstore):
+    keys = set(blobstore.keys())
     missing = {}
     for name, value in data['items'].items():
-        if value not in blobstore:
+        if value not in keys:
             missing[name] = value
     if missing:
         # cause we want None else
