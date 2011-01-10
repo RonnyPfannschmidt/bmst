db8bd820200e271748da2553afc0b5abb2fff2be
Ronny Pfannschmidt <Ronny.Pfannschmidt@gmx.de>
2011-01-10 18:27:13 +0100

    add simple timestamps

--- testing/test_backup_app.py
+++ testing/test_backup_app.py
@@ -1,6 +1,7 @@
+import bz2
+import json
 from bmst.backup_app import load_tree, fullmeta, make_backup
 from bmst.managed import  BMST
-import bz2
 
 
 def test_load(tmpdir):
@@ -20,6 +21,7 @@ def test_fullmeta(tmpdir):
     assert meta == {
         'project': 'root',
         'tags': ['backup'],
+        'timestamp': meta['timestamp'],  # steal timestamp
         'items': {
             'test': '4e1243bd22c66e76c2ba9eddc1f91394e57f9f83',
         },
@@ -39,12 +41,14 @@ def test_makebackup(tmpdir):
             b'BZh91AY&SY\xcc\xc3q\xd4\x00\x00\x02A\x80\x00\x10\x02\x00\x0c\x00'
             ' \x00!\x9ah3M\x19\x97\x8b\xb9"\x9c(Hfa\xb8\xea\x00',
     }
-    assert bmst.meta == {
-        '675857289b324a4f65e1fc5ead1a97d75a248945':
-            b'BZh91AY&SY(\xe2\xd0f\x00\x00?\xdb\x80@\x10P\x04?\xf0\x00\n?\xba'
-            '\xde\n \x00t"\x9az\x02z\x9a1\x03\xd4h\x0c\x83S\xd2CC \xd0\x1ah\r'
-            '\x04\x17z\x10N\x1e\x81\x80\x80\xc5)\xe8\t$\x9e\x9d%\x91 \xe5\x80'
-            '~lt\xda8&F:\x0bX\xd6\x19\x89\xcc%\xc5\xae\x00\x88L3zY\x12K\x89'
-            '\xaa\xb7\x10_p Das\xfe\x91P\x06\xaa\xdd\x1e\xd5\x8b\x9b\x97DT'
-            '\xf9G\x9f\x8b\xb9"\x9c(H\x14qh3\x00',
+    key, = bmst.meta.keys()
+    data = bz2.decompress(bmst.meta[key])
+    meta = json.loads(data)
+    assert meta == {
+        'project': 'root',
+        'tags': ['backup'],
+        'timestamp': meta['timestamp'],  # steal timestamp
+        'items': {
+            'test': '4e1243bd22c66e76c2ba9eddc1f91394e57f9f83',
+        },
     }
