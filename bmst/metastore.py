655508e58ac76d9a889622a298fc1776ec7ca32b
Ronny Pfannschmidt <opensource@ronnypfannschmidt.de>
2011-01-12 07:59:22 +0100

    move the missing blob finding function to the manager

--- bmst/metastore.py
+++
@@ -1,9 +0,0 @@
-def find_missing_blobs(data, blobstore):
-    keys = set(blobstore.keys())
-    missing = {}
-    for name, value in data['items'].items():
-        if value not in keys:
-            missing[name] = value
-    if missing:
-        # cause we want None else
-        return missing
