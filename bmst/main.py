76cc80f7a914789880790735e935680913036c90
Ronny Pfannschmidt <opensource@ronnypfannschmidt.de>
2011-01-13 17:16:49 +0100

    complete the store check command

--- bmst/main.py
+++ bmst/main.py
@@ -8,10 +8,11 @@ parser.convert_arg_line_to_args = shlex.split
 
 parser.add_argument('-s', '--store', required=True)
 parser.add_argument('-d', '--debug', action='store_true')
+parser.add_argument('-c', '--check', action='store_true')
 parser.add_argument('--backup', default=[], action='append')
 
 from bmst.backup_app import make_backup
-from bmst.managed import BMST
+from bmst.managed import BMST, check_bmst
 from bmst.store import FileStore
 
 
@@ -22,6 +23,9 @@ def main():
     bmst = py.path.local(opts.store)
     print('using store', bmst)
     bmst = get_bmst(bmst)
+
+    if opts.check:
+        check_bmst(bmst)
 
     for to_backup in opts.backup:
         path = py.path.local(to_backup)
