5c6dd59fcb2d39e77ee955ab76c568af0c3d8882
Ronny Pfannschmidt <opensource@ronnypfannschmidt.de>
2019-09-26 20:55:34 +0200

    add midding wsgi_intercept dependency

--- setup.py
+++ setup.py
@@ -6,5 +6,5 @@ setup(
     description="a simple experimet in backup stores",
     use_scm_version=True,
     setup_requires=["setuptools_scm>3"],
-    install_requires=["httplib2", "werkzeug", "py", "attr"],
+    install_requires=["httplib2", "werkzeug", "py", "attr", "wsgi_intercept"],
 )
